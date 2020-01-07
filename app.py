30379e6c35c47fda684b0a137105dc9eb36a682c
radui1398 <radui1398@gmail.com>
2020-01-07 21:53:06 +0200

    Readme Update 2

--- app.py
+++ app.py
@@ -6,12 +6,22 @@ from api.process_xml import *
 app = Flask(__name__)
 env = init_modules()
 
+
 @app.route('/upload_file', methods=['POST'])
 def upload_file():
     if not request.json or not ('file_name' in request.json and 'content' in request.json):
         return {'error': 'Invalid request body'}, 400
 
     return process_xml(env, request.json['content'], request.json['file_name'])
+
+
+# Get document by tag
+@app.route('/tags/<tag_name>', methods=['GET'])
+def get_by_tag():
+    tag_name = request.query_string.tag_named
+
+    return 200
+
 
 if __name__ == '__main__':
     app.run(debug=True)

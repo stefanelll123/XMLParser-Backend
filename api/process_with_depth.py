448a73ce93fdc108b754392623357665887a6471
Radu Ionut <radui1398@gmail.com>
2020-01-07 23:05:07 +0200

    small fix

--- api/process_with_depth.py
+++ api/process_with_depth.py
@@ -9,8 +9,6 @@ def get_docs_with_depth(env, depth):
         # searching by tag_name and converting cursor to list
         docs = list(mongo.meta.find({'max_depth': {'$gte': int(depth)}}, {'content': 1, 'file_name': 1, '_id': 0}))
 
-        print(docs)
-
         return {'status': 0, 'docs': docs}, 200
     except:
         return {'error': 'Database connection failed!'}, 500
--- api/process_with_tags.py
+++ api/process_with_tags.py
@@ -9,8 +9,6 @@ def get_docs_with_tag_name(env, tag_name):
         # searching by tag_name and converting cursor to list
         docs = list(mongo.meta.find({'attributes': {'$eq': tag_name}}, {'content': 1, 'file_name': 1, '_id': 0}))
 
-        print(docs)
-
         return {'status': 0, 'docs': docs}, 200
     except:
         return {'error': 'Database connection failed!'}, 500

b452c11fddadbd2ef7749b0d22d5936af9ed92a9
MihaelaPotlog <mihaela.potlog@info.uaic.ro>
2020-01-07 23:17:34 +0200

    get docs by size

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from modules.init import init_modules
+from api.get_docs_by_size import *
 
 from api.process_xml import *
 
@@ -12,6 +13,12 @@ def upload_file():
         return {'error': 'Invalid request body'}, 400
 
     return process_xml(env, request.json['content'], request.json['file_name'])
+
+@app.route('/elements', methods=['GET'])
+def get_by_size():
+    size = request.args.get("size")
+    return get_docs_by_size(env, size)
+
 
 if __name__ == '__main__':
     app.run(debug=True)

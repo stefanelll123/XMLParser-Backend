9f785607fee31b201c39af882b7f4a8cad59c9e5
Victor PULBERE <vpulbere@bitdefender.com>
2020-01-15 20:25:47 +0200

    Added paths

--- api/get_docs_by_size.py
+++ api/get_docs_by_size.py
@@ -4,7 +4,12 @@ def get_docs_by_size(env, size):
         docs = mongo.meta.find({"size": int(size)}, {"file_name": 1, "_id": 1})
         new_docs = list()
         for doc in docs:
-            new_docs.append({"_id": str(doc.get("_id")), "file_name": doc.get("file_name")})
+            file_id = str(doc.get("_id"))
+            new_docs.append({
+                "_id": file_id, 
+                "file_name": doc.get("file_name"),
+                'path': f'/files?file_id={file_id}'
+            })
     except:
         return {'error': 'Could not query'}, 500
 

d8bff43b810679b3f196e7937c798fa8deca635f
MihaelaPotlog <mihaela.potlog@info.uaic.ro>
2020-01-15 18:13:58 +0200

    add-id

--- api/get_docs_by_size.py
+++ api/get_docs_by_size.py
@@ -1,8 +1,11 @@
 def get_docs_by_size(env, size):
     try:
         mongo = env.get('mongo')
-        docs = mongo.meta.find({"size": int(size)}, {"file_name": 1, "_id": 0})
+        docs = mongo.meta.find({"size": int(size)}, {"file_name": 1, "_id": 1})
+        new_docs = list()
+        for doc in docs:
+            new_docs.append({"_id": str(doc.get("_id")), "file_name": doc.get("file_name")})
     except:
         return {'error': 'Could not query'}, 500
 
-    return {'docs': list(docs)}, 200
+    return {'docs': new_docs}, 200
